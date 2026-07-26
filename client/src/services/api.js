4d951279aa55d15cb01e2bdac2457abd4186fe70
carlosdeuganda <carlosdeuganda@gmail.com>
2026-07-26 04:55:04 +0300

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Base API configuration
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://e-commerce-owv6.onrender.com/api';
+
 
 const api = axios.create({
   baseURL: API_URL,
